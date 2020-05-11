d30ef46d04eb23e7f1060ef872a5f68993eed1be
DianWoung <dian126@foxmail.com>
2020-05-11 10:18:30 +0800

    first commit

---
+++ app/Controller/IndexController.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * This file is part of Hyperf.
+ *
+ * @link     https://www.hyperf.io
+ * @document https://doc.hyperf.io
+ * @contact  group@hyperf.io
+ * @license  https://github.com/hyperf-cloud/hyperf/blob/master/LICENSE
+ */
+
+namespace App\Controller;
+
+use Elasticsearch\Endpoints\DeleteByQuery;
+use Hyperf\DbConnection\Db;
+use Hyperf\Utils\Collection;
+
+class IndexController extends AbstractController
+{
+    public function index()
+    {
+
+        $res = Db::table("goods_order")
+            ->where([
+                "order_status" => 2,
+            ])->limit(1000)
+            ->select("order_id")
+            ->get();
+
+        return [
+            'data' => $res,
+        ];
+    }
+}
